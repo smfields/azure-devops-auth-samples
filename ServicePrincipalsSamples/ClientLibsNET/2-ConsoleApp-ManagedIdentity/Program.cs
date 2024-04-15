30b051a1d67c57c7648a62c48dfd3a0e6dafb4b2
Sam Fields <samfields@microsoft.com>
2024-04-15 09:26:40 -0400

    Update script

--- ServicePrincipalsSamples/ClientLibsNET/2-ConsoleApp-ManagedIdentity/Program.cs
+++ ServicePrincipalsSamples/ClientLibsNET/2-ConsoleApp-ManagedIdentity/Program.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Core;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Azure.Core;
 using Azure.Identity;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.VisualStudio.Services.Client;
@@ -9,11 +10,11 @@ namespace ServicePrincipalsSamples
 {
     public static class Program
     {
-        public const string AdoBaseUrl = "https://dev.azure.com";
-
-        public const string AdoOrgName = "Your organization name";
-
-        public const string AadTenantId = "Your Azure AD tenant id";
+        public const string AdoBaseUrl = "https://microsoft.visualstudio.com/";
+        public const string AdoOrgName = "OS";
+        public static readonly TimeSpan Delay = TimeSpan.FromMinutes(5);
+        
+        public const string AadTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
         // ClientId for User Assigned Managed Identity. Leave null for System Assigned Managed Identity
         public const string AadUserAssignedManagedIdentityClientId = null;
 
@@ -25,31 +26,56 @@ namespace ServicePrincipalsSamples
 
         public static async Task Main()
         {
-            Console.Write("Work item ID: ");
-            int workItemId = Convert.ToInt32(Console.ReadLine());
+            int workItemId = 49815058;
+            Console.WriteLine($"Work item ID: {workItemId}");
 
-            var vssConnection = await CreateVssConnection();
-
-            var workItemTrackingHttpClient = vssConnection.GetClient<WorkItemTrackingHttpClient>();
-            var workItem = await workItemTrackingHttpClient.GetWorkItemAsync(workItemId);
-
-            Console.WriteLine($"Work Item Title: {workItem.Fields["System.Title"]}");
-        }
-
-        private static async Task<VssConnection> CreateVssConnection()
-        {
+            Console.WriteLine("Getting token");
             var accessToken = await GetManagedIdentityAccessToken();
-            var token = new VssAadToken("Bearer", accessToken);
+            var token = new VssAadToken("Bearer", accessToken.Token);
             var credentials = new VssAadCredential(token);
+            var connection = new VssConnection(new Uri(AdoBaseUrl), credentials);
 
-            var settings = VssClientHttpRequestSettings.Default.Clone();
-            settings.UserAgent = AppUserAgent;
+            var attemptNumber = 1;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($" --- Attempt {attemptNumber} ---");
+                    Console.WriteLine($"Time to Live: {accessToken.ExpiresOn - DateTimeOffset.UtcNow}");
 
-            var organizationUrl = new Uri(new Uri(AdoBaseUrl), AdoOrgName);
-            return new VssConnection(organizationUrl, credentials, settings);
+                    Console.WriteLine("Getting HTTP Client");
+                    var workItemTrackingHttpClient = await connection.GetClientAsync<WorkItemTrackingHttpClient>();
+
+                    Console.WriteLine("Getting work item");
+                    var workItem = await workItemTrackingHttpClient.GetWorkItemAsync(workItemId);
+
+                    Console.WriteLine($"Work Item Title: {workItem.Fields["System.Title"]}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    Console.WriteLine($" --- Attempt {attemptNumber++} Complete ---");
+                    Console.WriteLine($"Next attempt at: {DateTimeOffset.Now + Delay}");
+                    await Task.Delay(Delay);
+                }
+            }
         }
 
-        private static async Task<string> GetManagedIdentityAccessToken()
+        // private static async Task<VssConnection> CreateVssConnection()
+        // {
+        //
+        //
+        //     var settings = VssClientHttpRequestSettings.Default.Clone();
+        //     settings.UserAgent = AppUserAgent;
+        //
+        //     var organizationUrl = new Uri(new Uri(AdoBaseUrl), AdoOrgName);
+        //     return new VssConnection(organizationUrl, credentials, settings);
+        // }
+
+        private static async Task<AccessToken> GetManagedIdentityAccessToken()
         {
             // DefaultAzureCredential will use VisualStudioCredentials or other appropriate credentials for local development
             // but will use ManagedIdentityCredential when deployed to an Azure Host with Managed Identity enabled.
@@ -66,7 +92,7 @@ namespace ServicePrincipalsSamples
             var tokenRequestContext = new TokenRequestContext(VssAadSettings.DefaultScopes);
             var token = await credential.GetTokenAsync(tokenRequestContext, CancellationToken.None);
 
-            return token.Token;
+            return token;
         }
     }
 }
